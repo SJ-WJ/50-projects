2289b6bf510ca288a3616f3ea559539d9596006a
SJ-WJ <sirajwahaj.ibrahim@gmail.com>
2023-03-27 18:00:00 -0600

    Finishing project 18, completing the dark mode and clock function

---
+++ 19_theme-clock/script.js
@@ -0,0 +1,74 @@
+const hourElement = document.querySelector('.hour');
+const minElement = document.querySelector('.min');
+const secondElement = document.querySelector('.second');
+const timeElement = document.querySelector('.time');
+const dateElement = document.querySelector('.date');
+const toggle = document.querySelector('.toggle');
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Satday"
+];
+
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+
+toggle.addEventListener('click', (e) => {
+  const html = document.querySelector('html');
+
+  if (html.classList.contains('dark')) {
+    html.classList.remove('dark');
+    e.target.innerHTML = 'Dark mode';
+
+  } else {
+    html.classList.add('dark');
+    e.target.innerHTML = 'Light mode'
+  }
+
+});
+
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hours = time.getHours();
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const amOrPm = hours >= 12 ? 'PM' : 'AM';
+
+  hourElement.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+  minElement.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
+  secondElement.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+
+  timeElement.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}`: minutes} ${amOrPm}`;
+  dateElement.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+
+}
+
+// From stackoverflow
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime();
+
+setInterval(setTime, 1000);
